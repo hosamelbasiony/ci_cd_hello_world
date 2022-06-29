11cf5c219b528fe0e2fbeaddfd602fdb935672c9
Hosam Al-Basiony <hosam.el.basiony@gmail.com>
2022-06-29 20:33:41 +0200

    ='update'

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-use-before-define */
+
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
